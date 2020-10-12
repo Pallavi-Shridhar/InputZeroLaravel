42cc88231a3b2d815d73250b20afdbb39c2cf3f8
PallaviShridhar <pallavi@innovexic.com>
2020-10-12 12:58:10 +0530

    Validations added input_zero.sql.zip added

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,20 +8,27 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    //User details API for fetching the data of each user
     public function details(Request $request){
         $user = User::select('id','name','email','phone','city')
                     ->where('id',$request->id)
                     ->first();
-
-        if(!empty($request->fmt)){
-            return $user;
-        }else {
+        //Validation if the user exists or not
+        if(!empty($user)){
+            if(!empty($request->fmt)){
+                return $user;
+            }else {
+                return response()->json([
+                    'user' => $user,
+                    'message' => 'success',
+                    'status' => 'TRUE'
+                ], 200);
+            }
+        } else {
             return response()->json([
-                'user' => $user,
-                'message' => 'success',
-                'status' => 'TRUE'
-            ], 200);
-        }
-        
+                'message' => 'User does not exist',
+                'status' => 'False'
+            ], 400);
+        }        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Routing to the User details API
 Route::post('user-details', 'Api\UserController@details');
